35d51bb3117ef3b3decad14571d1a1fe48dff09e
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-03-09 12:44:50 -0700

    Agregue las nuevas opciones

--- server.js
+++ server.js
@@ -54,13 +54,17 @@ app.post('/api/add-option', (req, res) => {
 
 // In-memory database for evaluations
 let evaluationOptions = [
-    { id: 1, name: 'Pescado a la Talla (Arroz Blanco y Ensalada)', ratings: [] },
-    { id: 2, name: 'Domino\'s Pizza', ratings: [] },
-    { id: 3, name: 'Chilaquiles Rojos', ratings: [] },
-    { id: 4, name: 'BBQ City', ratings: [] },
-    { id: 5, name: 'Malteada de Vainilla', ratings: [] },
-    { id: 6, name: 'Fruta', ratings: [] },
-    { id: 7, name: 'Botanita', ratings: [] }
+    { id: 1, name: 'Tacos de Pescado estilo Ensenada', ratings: [] },
+    { id: 2, name: 'Sushi Tu Rollo Cinco', ratings: [] },
+    { id: 3, name: 'Burritos Machacado', ratings: [] },
+    { id: 4, name: 'Chick N Crunch', ratings: [] },
+    { id: 5, name: 'Quesadilla y Carne/Chocomilk', ratings: [] },
+    { id: 6, name: 'Chilaquiles Rojos', ratings: [] },
+    { id: 7, name: 'Ceviche Rugus/Nieve de Fresa', ratings: [] },
+    { id: 8, 'name': 'Domino\'s Pizza', ratings: [] },
+    { id: 9, name: 'Huevito de Ensalada', ratings: [] },
+    { id: 10, name: 'Fruta', ratings: [] },
+    { id: 11, name: 'Botanita', ratings: [] }
 ];
 
 // API: Get evaluations
